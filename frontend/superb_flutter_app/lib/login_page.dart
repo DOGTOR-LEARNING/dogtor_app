33fb68e8247a41c89973e1f217df3c53dd4c7b70
ckhsc17 <94750792+Bowenbowenbowenbowen@users.noreply.github.com>
2025-03-22 02:12:10 +0800

    modify_gitignore

--- frontend/superb_flutter_app/lib/login_page.dart
+++ frontend/superb_flutter_app/lib/login_page.dart
@@ -1,23 +1,26 @@
-// frontend/superb_flutter_app/lib/login_page.dart
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'home_page.dart';
 
 class LoginPage extends StatelessWidget {
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  // 使用您的 Google 客戶端 ID
+  final String clientId = '426092249907-e5ff9jmpceiads6n4sfkof2uemjcrhm5.apps.googleusercontent.com';
+  
+  // 初始化 GoogleSignIn，並傳遞 clientId
+  final GoogleSignIn _googleSignIn = GoogleSignIn(
+    clientId: '426092249907-e5ff9jmpceiads6n4sfkof2uemjcrhm5.apps.googleusercontent.com',
+  );
+  
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Future<void> _handleSignIn(BuildContext context) async {
     try {
       // 使用 Google 登入
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-      
-      // 檢查 googleUser 是否為 null
       if (googleUser == null) {
-        // 用戶取消了登入，您可以選擇顯示一條消息或執行其他操作
         print("Google sign-in was canceled.");
-        return; // 退出方法
+        return; // 用戶取消了登入
       }
 
       final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
