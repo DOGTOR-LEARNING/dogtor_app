26d3dcb3a6043e4d0af2985588a229ebc76a1a10
Guanyu Chen <b12705058@g.ntu.edu.tw>
2025-08-04 16:47:36 +0800

    Chem path

--- backend/agent/4_import_level_info.py
+++ backend/agent/4_import_level_info.py
@@ -188,7 +188,7 @@ def update_csv_with_level_ids(csv_file_path):
 
 if __name__ == "__main__":
     # CSV 檔案路徑
-    csv_file_path = "high_chem_level_info.csv" # 要改這個路徑！！！！
+    csv_file_path = "../../frontend/superb_flutter_app/assets/edu_data/level_info/jun_math_level.csv" # 要改這個路徑！！！！
     
     # 選擇操作模式
     mode = input("選擇操作模式：1. 導入關卡資訊並更新 CSV  2. 僅更新 CSV 中的 level_id：")
